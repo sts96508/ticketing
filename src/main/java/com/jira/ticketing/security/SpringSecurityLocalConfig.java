6e4566f6715f8954565b3522426b437b37836355
sid <sriwastav@gmail.com>
2024-07-01 06:04:19 +0100

    removed .noai

--- src/main/java/com/jira/ticketing/security/SpringSecurityLocalConfig.java
+++ src/main/java/com/jira/ticketing/security/SpringSecurityLocalConfig.java
@@ -7,4 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 public class SpringSecurityLocalConfig {
+
 }
